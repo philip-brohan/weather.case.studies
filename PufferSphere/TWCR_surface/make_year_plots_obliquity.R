f2ccc8b379dcea3f2fe1e567d8352028a8249c80
Philip Brohan <philip@brohan.org>
2016-03-18 12:54:50 +0000

    Remove precip - doesn't show up well on sphere.

--- PufferSphere/TWCR_surface/make_year_plots_obliquity.R
+++ PufferSphere/TWCR_surface/make_year_plots_obliquity.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript --no-save
 
-# Show weather and fog in Gil's scout reanalysis.
+# Show wind and temperature in 20CR2c
 # Spherical version with garish colours
 # Move the north pole to centre UK - shows up better on PufferSphere?
 
@@ -10,12 +10,12 @@ library(chron)
 library(parallel)
 
 Year<-2014
-Month<-5
-Day<-1
+Month<-1
+Day<-30
 Hour<-0
-n.total<-0#365*24
+n.total<-365*24*3
 version<-'3.5.1'
-cores<-6
+cores<-8
 
 fog.threshold<-exp(1)
 
@@ -41,18 +41,20 @@ Options<-WeatherMap.set.option(Options,'show.mslp',F)
 Options<-WeatherMap.set.option(Options,'show.ice',T)
 Options<-WeatherMap.set.option(Options,'show.obs',F)
 Options<-WeatherMap.set.option(Options,'show.fog',F)
-Options<-WeatherMap.set.option(Options,'show.precipitation',T)
+Options<-WeatherMap.set.option(Options,'show.precipitation',F)
 Options<-WeatherMap.set.option(Options,'temperature.range',25)
 Options<-WeatherMap.set.option(Options,'wind.palette',
                        diverge_hcl(7,c=c(150,0),l=c(25,30),power=1))
 Options<-WeatherMap.set.option(Options,'obs.size',1.5)
-Options<-WeatherMap.set.option(Options,'land.colour',rgb(0,0,0,255,
-                                                       maxColorValue=255))
-Options<-WeatherMap.set.option(Options,'sea.colour',rgb(100,100,100,255,
-                                                       maxColorValue=255))
+#Options<-WeatherMap.set.option(Options,'land.colour',rgb(0,0,0,255,
+#                                                       maxColorValue=255))
+#Options<-WeatherMap.set.option(Options,'sea.colour',rgb(100,100,100,255,
+#                                                       maxColorValue=255))
 Options<-WeatherMap.set.option(Options,'obs.colour',rgb(255,215,0,255,
                                                        maxColorValue=255))
-Options<-WeatherMap.set.option(Options,'precip.colour',c(0,0.1,0))
+Options<-WeatherMap.set.option(Options,'precip.colour',c(0.1,0.2,0.1))
+Options<-WeatherMap.set.option(Options,'precip.range',0.01)
+#Options<-WeatherMap.set.option(Options,'precip.points',10000)
 Options<-WeatherMap.set.option(Options,'pole.lon',160)
 Options<-WeatherMap.set.option(Options,'pole.lat',35)
 Options<-WeatherMap.set.option(Options,'background.resolution','high')
@@ -60,21 +62,22 @@ Options<-WeatherMap.set.option(Options,'background.resolution','high')
 aspect<-2
 
 Options$ice.points<-100000
-Options$wind.vector.lwd<-2
+Options$wind.vector.lwd<-2.5
+Options$wind.vector.move.scale<-Options$wind.vector.move.scale/3
 Options$wind.vector.density<-Options$wind.vector.density*0.5
 land<-WeatherMap.get.land(Options)
 
 make.streamlines<-function(year,month,day,hour,streamlines=NULL) {
 
 
-    sf.name<-sprintf("%s/streamlines.%04d-%02d-%02d:%02d.rd",
-                           Imagedir,year,month,day,hour)
+    sf.name<-sprintf("%s/streamlines.%04d-%02d-%02d:%02d:%02d.rd",
+                           Imagedir,year,month,day,as.integer(hour),as.integer((hour%%1)*60))
     if(file.exists(sf.name) && file.info(sf.name)$size>5000) {
        load(sf.name)
        return(s)
     }
-    print(sprintf("%04d-%02d-%02d:%02d - %s",year,month,day,hour,
-                   Sys.time()))
+    print(sprintf("%04d-%02d-%02d:%02d:%02d - %s",year,month,day,
+                   as.integer(hour),as.integer((hour%%1)*60),Sys.time()))
 
     uwnd<-TWCR.get.slice.at.hour('uwnd.10m',year,month,day,hour,version=version)
     vwnd<-TWCR.get.slice.at.hour('vwnd.10m',year,month,day,hour,version=version)
@@ -91,14 +94,12 @@ make.streamlines<-function(year,month,day,hour,streamlines=NULL) {
 plot.hour<-function(year,month,day,hour,streamlines) {
 
 
-    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+    image.name<-sprintf("%04d-%02d-%02d:%02d:%02d.png",year,month,day,
+                               as.integer(hour),as.integer((hour%%1)*60))
 
     ifile.name<-sprintf("%s/%s",Imagedir,image.name)
     if(file.exists(ifile.name) && file.info(ifile.name)$size>0) return()
 
-    prmsl<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version=version)
-    prmsl.normal<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version='3.4.1',
-                                             type='normal')
     prate<-NULL
     if(Options$show.precipitation) {
        prate<-TWCR.get.slice.at.hour('prate',year,month,day,hour,version=version)
@@ -113,7 +114,7 @@ plot.hour<-function(year,month,day,hour,streamlines) {
              type='cairo')
     Options$label<-sprintf("")
        WeatherMap.draw(Options=Options,uwnd=NULL,icec=icec,
-                          vwnd=NULL,precip=prate,mslp=prmsl,
+                          vwnd=NULL,precip=prate,mslp=NULL,
                           t.actual=NULL,t.normal=NULL,land=land,
                           fog=NULL,obs=NULL,streamlines=streamlines)
     dev.off()
@@ -123,23 +124,23 @@ s<-NULL
 jobs<-list()
 for(n.count in seq(0,n.total)) {
 
-    n.date<-c.date+n.count/24
+    n.date<-c.date+n.count/(24*3)
     year<-as.numeric(as.character(years(n.date)))
     if(year>2014) next
     month<-months(n.date)
     day<-days(n.date)
-    hour<-(n.count+Hour)%%24
+    hour<-((n.count+Hour)%%(24*3))/3
 
     # serial component - streamlines evolve from hour to hour
     s<-make.streamlines(year,month,day,hour,streamlines=s)
 
-    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+    image.name<-sprintf("%04d-%02d-%02d:%02d:%02d.png",year,month,day,
+                               as.integer(hour),as.integer((hour%%1)*60))
+
     ifile.name<-sprintf("%s/%s",Imagedir,image.name)
     if(file.exists(ifile.name) && file.info(ifile.name)$size>0) next
 
     # Each plot in a seperate parallel process
-    plot.hour(year,month,day,hour,s)
-    q('no')
     mcparallel(plot.hour(year,month,day,hour,s))
     if(n.count%%cores==0) mccollect(wait=TRUE)
 
