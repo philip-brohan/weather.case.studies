fd9dfea2415ff859bdd37f72b31120e2e03d9bec
Philip Brohan <philip@brohan.org>
2016-10-04 17:36:26 +0100

    Refactor the puffersphere scripts for easier re-use.

---
+++ PufferSphere/TWCR_pressure+ice/static/P+I_single.R
@@ -0,0 +1,139 @@
+#!/usr/bin/Rscript
+
+# Pressure and ice only - for the PufferSphere
+# Run a single timestep only - designed to be run in parallel on SPICE.
+
+library(GSDF.TWCR)
+library(GSDF.WeatherMap)
+library(parallel)
+library(getopt)
+
+opt = getopt(c(
+  'step',   's', 2, "integer"
+));
+if ( is.null(opt$step  ) )   { stop("Step not specified") }
+
+Year<-2014
+Month<-1
+Day<-2
+Hour<-0
+version<-'3.5.1'
+
+Imagedir<-sprintf("%s/images/P+I",Sys.getenv('SCRATCH'))
+if(!file.exists(Imagedir)) dir.create(Imagedir,recursive=TRUE)
+
+c.date<-chron(dates=sprintf("%04d/%02d/%02d",Year,Month,Day),
+          times=sprintf("%02d:00:00",Hour),
+          format=c(dates='y/m/d',times='h:m:s'))
+
+Options<-WeatherMap.set.option(NULL)
+
+Options<-WeatherMap.set.option(Options,'lat.min',-90)
+Options<-WeatherMap.set.option(Options,'lat.max',90)
+Options<-WeatherMap.set.option(Options,'lon.min',-190)
+Options<-WeatherMap.set.option(Options,'lon.max',190)
+Options$vp.lon.min<- -180
+Options$vp.lon.max<-  180
+Options<-WeatherMap.set.option(Options,'pole.lon',160)
+Options<-WeatherMap.set.option(Options,'pole.lat',35)
+Options<-WeatherMap.set.option(Options,'background.resolution','high')
+Options<-WeatherMap.set.option(Options,'land.colour',rgb(0,0,0,255,
+                                                       maxColorValue=255))
+Options<-WeatherMap.set.option(Options,'sea.colour',rgb(100,100,100,255,
+                                                       maxColorValue=255))
+Options$mslp.base=101325                    # Base value for anomalies
+Options$mslp.range=50000                    # Anomaly for max contour
+Options$mslp.step=500                       # Smaller -> more contours
+Options$mslp.tpscale=500                    # Smaller -> contours less transparent
+Options$mslp.lwd=4
+
+Options$ice.points<-100000
+land<-WeatherMap.get.land(Options)
+
+Draw.pressure<-function(mslp,Options,colour=c(0,0,0)) {
+
+  M<-GSDF.WeatherMap:::WeatherMap.rotate.pole(mslp,Options)
+  M<-GSDF:::GSDF.pad.longitude(M) # Extras for periodic boundary conditions
+  lats<-M$dimensions[[GSDF.find.dimension(M,'lat')]]$values
+  longs<-M$dimensions[[GSDF.find.dimension(M,'lon')]]$values
+    # Need particular data format for contourLines
+  if(lats[2]<lats[1] || longs[2]<longs[1] || max(longs) > 182 ) {
+    if(lats[2]<lats[1]) lats<-rev(lats)
+    if(longs[2]<longs[1]) longs<-rev(longs)
+    longs[longs>182]<-longs[longs>182]-364
+    longs<-sort(longs)
+    M2<-M
+    M2$dimensions[[GSDF.find.dimension(M,'lat')]]$values<-lats
+    M2$dimensions[[GSDF.find.dimension(M,'lon')]]$values<-longs
+    M<-GSDF.regrid.2d(M,M2)
+  }
+  z<-matrix(data=M$data,nrow=length(longs),ncol=length(lats))
+  contour.levels<-seq(Options$mslp.base-Options$mslp.range,
+                      Options$mslp.base+Options$mslp.range,
+                      Options$mslp.step)
+  lines<-contourLines(longs,lats,z,
+                       levels=contour.levels)
+  if(!is.na(lines) && length(lines)>0) {
+     for(i in seq(1,length(lines))) {
+         tp<-min(1,(abs(lines[[i]]$level-Options$mslp.base)/
+                    Options$mslp.tpscale))
+         lt<-2
+         lwd<-1
+         if(lines[[i]]$level<=Options$mslp.base) {
+             lt<-1
+             lwd<-1
+         }
+         gp<-gpar(col=rgb(colour[1],colour[2],colour[3],tp),
+                             lwd=Options$mslp.lwd*lwd,lty=lt)
+         grid.xspline(x=unit(lines[[i]]$x,'native'),
+                    y=unit(lines[[i]]$y,'native'),
+                    shape=1,
+                    gp=gp)
+     }
+  }
+}
+
+plot.hour<-function(year,month,day,hour) {    
+
+    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+
+    ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+    if(file.exists(ifile.name) && file.info(ifile.name)$size>0) return()
+
+    prmsl.T<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version=version)
+    icec<-TWCR.get.slice.at.hour('icec',year,month,day,hour,version=version)
+
+     png(ifile.name,
+             width=1050*WeatherMap.aspect(Options),
+             height=1050,
+             bg=Options$sea.colour,
+             pointsize=24,
+             type='cairo')
+       pushViewport(dataViewport(c(Options$vp.lon.min,Options$vp.lon.max),
+                                 c(Options$lat.min,Options$lat.max),
+                                  extension=0))
+          ip<-WeatherMap.rectpoints(Options$ice.points,Options)
+          WeatherMap.draw.ice(ip$lat,ip$lon,icec,Options)
+          WeatherMap.draw.land(land,Options)
+
+        Draw.pressure(prmsl.T,Options,colour=c(0.91,0.46,0))
+
+     upViewport()
+
+    dev.off()
+}
+
+n.count<-opt$step
+
+    n.date<-c.date+n.count/24
+    year<-as.numeric(as.character(years(n.date)))
+    month<-months(n.date)
+    day<-days(n.date)
+    hour<-n.count%%24
+
+    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+    ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+    if(file.exists(ifile.name) && file.info(ifile.name)$size>0) next
+    plot.hour(year,month,day,hour)
+
+    system(sprintf("mogrify -gamma 0.6 %s",ifile.name))
