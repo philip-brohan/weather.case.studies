d2591b60d038f0d05b5d3baaf5a44e7fe8ab3fc5
Philip Brohan <philip@brohan.org>
2016-11-15 11:32:26 +0000

    Finalised ERA5 test climatology script.

--- get_data_from_ECMWF/ERA5_fake_climatology.R
+++ get_data_from_ECMWF/ERA5_fake_climatology.R
@@ -4,9 +4,9 @@
 library(GSDF.ERA5)
 library(lubridate)
 
-var<-'air.2m'
+var<-'prmsl'
 
-for(hour in seq(0,0)) {
+for(hour in seq(0,23)) {
 
    c.date<-ymd("2016-01-01")
    c<-ERA5.get.slice.at.hour(var,lubridate::year(c.date),
@@ -25,5 +25,5 @@ for(hour in seq(0,0)) {
    }
    c$data[]<-c$data/count
    saveRDS(c,sprintf("%s/ERA5/oper/climtologies.test/%s.%02d.Rdata",
-                     Sys.getenv("SCRATCH"),hour,var))
+                     Sys.getenv("SCRATCH"),var,hour))
 }
