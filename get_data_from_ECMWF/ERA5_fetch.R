cddc3779d3ae5c0720fad18a9bc4db3c00fcb3ec
Philip Brohan <philip@brohan.org>
2016-11-18 16:45:22 +0000

    Download ERA5 forecast data for 06 and 18 start times seperately.

--- get_data_from_ECMWF/ERA5_fetch.R
+++ get_data_from_ECMWF/ERA5_fetch.R
@@ -61,30 +61,32 @@ get.surface.forecast<-function(var) {
  fname<-tempfile()
  target.dir<-sprintf("%s/%s/hourly/%04d/%02d/%02d",local.dir,opt$stream,opt$year,opt$month,opt$day)
  if(!file.exists(target.dir)) dir.create(target.dir,recursive=TRUE)
- target.file<-(sprintf("%s/%s.nc",target.dir,var))
- if(file.exists(target.file) && file.info(target.file)$size>0) return() # already done
- sink(fname)
- cat('#!/usr/bin/env python\n')
- cat('from ecmwfapi import ECMWFDataServer\n')
- cat('server = ECMWFDataServer()\n')
- cat('server.retrieve({\n')
- cat('   \'dataset\'   : "era5_test",\n')
- cat(sprintf("   'stream'    : \"%s\",\n",opt$stream))
- cat('   \'type\'      : "fc",\n')
- cat('   \'step\'      : "1/to/12/by/1",\n')
- cat('   \'levtype\'   : "sfc",\n')
- cat(sprintf("   \'param\'     : \"%s\",\n",var))
- cat('   \'grid\'      : "0.25/0.25",\n')
- cat('   \'time\'      : "06/18",\n')
- cat(sprintf("   'date'      : \"%04d-%02d-%02d/to/%04d-%02d-%02d\",\n",
-                               opt$year,opt$month,opt$day,
-			       opt$year,opt$month,opt$day))
- cat('   \'format\'    : "netcdf",\n')
- cat(sprintf("   'target'    : \"%s\",\n",target.file))
- cat('})')
- sink()
- system(sprintf("python %s",fname))
- unlink(fname)
+ for(start.hour in c(6,18)) {
+     target.file<-(sprintf("%s/%s.%02d.nc",target.dir,var,start.hour))
+     if(file.exists(target.file) && file.info(target.file)$size>38000000) return() # already done
+     sink(fname)
+     cat('#!/usr/bin/env python\n')
+     cat('from ecmwfapi import ECMWFDataServer\n')
+     cat('server = ECMWFDataServer()\n')
+     cat('server.retrieve({\n')
+     cat('   \'dataset\'   : "era5_test",\n')
+     cat(sprintf("   'stream'    : \"%s\",\n",opt$stream))
+     cat('   \'type\'      : "fc",\n')
+     cat('   \'step\'      : "0/to/18/by/1",\n')
+     cat('   \'levtype\'   : "sfc",\n')
+     cat(sprintf("   \'param\'     : \"%s\",\n",var))
+     cat('   \'grid\'      : "0.25/0.25",\n')
+     cat(sprintf("   \'time\'      : \"%02d\",\n",start.hour))
+     cat(sprintf("   'date'      : \"%04d-%02d-%02d/to/%04d-%02d-%02d\",\n",
+                                   opt$year,opt$month,opt$day,
+                                   opt$year,opt$month,opt$day))
+     cat('   \'format\'    : "netcdf",\n')
+     cat(sprintf("   'target'    : \"%s\",\n",target.file))
+     cat('})')
+     sink()
+     system(sprintf("python %s",fname))
+     unlink(fname)
+ }
 }
 
 s<-lapply(vars,get.surface.forecast)
