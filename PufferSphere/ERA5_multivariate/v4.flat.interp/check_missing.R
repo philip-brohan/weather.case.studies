dea9c38d0644b319b7d6151b40454244942e1b59
Philip Brohan <philip@brohan.org>
2016-11-23 12:03:45 +0000

    Added missing ffmpeg script for ERAI blended vis. Also fixed the check_missing script for the ERA4v vis.

--- PufferSphere/ERA5_multivariate/v4.flat.interp/check_missing.R
+++ PufferSphere/ERA5_multivariate/v4.flat.interp/check_missing.R
@@ -8,11 +8,12 @@ current.day<-ymd("2016-01-02")
 end.day<-ymd("2016-02-28")
 
 while(current.day<=end.day) {
-  for(hour in seq(0,23)) {
-      file.name<-sprintf("%s/%04d-%02d-%02d:%02d.png",
+      for(hour in seq(0,23.75,0.25)) {
+      file.name<-sprintf("%s/%04d-%02d-%02d:%02d.%02d.png",
                          Imagedir,year(current.day),
                          month(current.day),
-                         day(current.day),hour)
+                         day(current.day),as.integer(hour),
+                         as.integer((hour%%1)*100))
       if(!file.exists(file.name)) {
         print(file.name)
       }
