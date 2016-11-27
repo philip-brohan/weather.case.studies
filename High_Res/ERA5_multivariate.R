9da62dde0678b187238ee38006fc05cc8bde3a64
Philip Brohan <philip@brohan.org>
2016-11-27 22:12:55 +0000

    Added pressure level fields to ERA5 retrieval script.

--- High_Res/ERA5_multivariate.R
+++ High_Res/ERA5_multivariate.R
@@ -8,7 +8,9 @@ library(GSDF.ERA5)
 library(GSDF.TWCR)
 library(GSDF.WeatherMap)
 library(grid)
-#library(extrafont)
+#library(RColorBrewer)
+#library(colorspace)
+library(extrafont)
 #loadfonts()
 
 opt = list(
@@ -21,16 +23,12 @@ opt = list(
 Imagedir<-sprintf(".",Sys.getenv('SCRATCH'))
 
 Options<-WeatherMap.set.option(NULL)
-Options<-WeatherMap.set.option(Options,'land.colour',rgb(180,180,180,255,
+Options<-WeatherMap.set.option(Options,'land.colour',rgb(50,50,50,255,
                                                        maxColorValue=255))
-Options<-WeatherMap.set.option(Options,'ice.colour',rgb(225,225,225,255,
+Options<-WeatherMap.set.option(Options,'sea.colour',rgb(200,200,200,255,
                                                        maxColorValue=255))
-Options<-WeatherMap.set.option(Options,'sea.colour',rgb(220,220,220,255,
+Options<-WeatherMap.set.option(Options,'ice.colour',rgb(250,250,250,255,
                                                        maxColorValue=255))
-Options$sea.colour=rgb(80*1.5,95*1.5,107*1.5,255,
-                  maxColorValue=255)
-Options$land.colour=rgb(123,121,117,255,
-                    maxColorValue=255)
 Options<-WeatherMap.set.option(Options,'background.resolution','high')
 Options<-WeatherMap.set.option(Options,'pole.lon',160)
 Options<-WeatherMap.set.option(Options,'pole.lat',45)
@@ -47,36 +45,75 @@ Options<-WeatherMap.set.option(Options,'wind.vector.points',3)
 Options<-WeatherMap.set.option(Options,'wind.vector.scale',0.2)
 Options<-WeatherMap.set.option(Options,'wind.vector.move.scale',1)
 Options<-WeatherMap.set.option(Options,'wind.vector.density',0.5)
-Options<-WeatherMap.set.option(Options,'wind.vector.lwd',0.25)
+Options<-WeatherMap.set.option(Options,'wind.vector.lwd',1.5)
 Options$ice.points<-100000
-Options<-WeatherMap.set.option(Options,'precip.min.transparency',0.6)
+Options<-WeatherMap.set.option(Options,'precip.min.transparency',0.9)
 Options<-WeatherMap.set.option(Options,'fog.min.transparency',0.0)
 
 Options$mslp.base=0                         # Base value for anomalies
 Options$mslp.range=50000                    # Anomaly for max contour
 Options$mslp.step=500                       # Smaller -> more contours
 Options$mslp.tpscale=500                    # Smaller -> contours less transparent
-Options$mslp.lwd=1
+Options$mslp.lwd=2
 Options$precip.colour=c(0,0.2,0)
 
-#WeatherMap.streamline.getGC<-function(value,transparency=NA,status=1,Options) {
-#   alpha<-c(10,50,150,255)[min(status,4)]
-#   return(gpar(col=rgb(25,25,25,alpha,maxColorValue=255),
-#               fill=rgb(125,125,125,alpha,maxColorValue=255),lwd=Options$wind.vector.lwd))
-#}
-#assignInNamespace("WeatherMap.streamline.getGC",WeatherMap.streamline.getGC, ns="GSDF.WeatherMap")
 
+WeatherMap.streamline.getGC<-function(value,transparency=NA,status=1,Options) {
+   status=4
+   alpha<-c(10,50,150,255)[min(status,4)]
+   return(gpar(col=rgb(125,125,125,alpha,maxColorValue=255),
+               fill=rgb(125,125,125,alpha,maxColorValue=255),lwd=Options$wind.vector.lwd))
+}
+assignInNamespace("WeatherMap.streamline.getGC",WeatherMap.streamline.getGC, ns="GSDF.WeatherMap")
+
+lsm<-GSDF.ncdf.load(sprintf("%s/ERA5/oper/fixed_land_mask.nc",Sys.getenv('SCRATCH')),'lsm',
+                             lat.range=c(-90,90),lon.range=c(0,360))
+orog<-GSDF.ncdf.load(sprintf("%s/ERA5/oper/fixed_geopotential.nc",Sys.getenv('SCRATCH')),'z',
+                             lat.range=c(-90,90),lon.range=c(0,360))
+w<-which(lsm$data==0)
+is.na(orog$data[w])<-TRUE
+
+draw.land<-function(Options,n.levels=20) {
+   land<-GSDF.WeatherMap:::WeatherMap.rotate.pole(GSDF:::GSDF.pad.longitude(orog),Options)
+   lons<-land$dimensions[[GSDF.find.dimension(land,'lon')]]$values
+   qtls<-quantile(land$data,probs=seq(0,1,1/n.levels),na.rm=TRUE)
+   base.colour<-col2rgb(Options$land.colour)/255
+   peak.colour<-c(.8,.8,.8)
+   for(level in seq_along(qtls)) {
+      if(level==1) next
+      fraction<-1-(level-1)/n.levels
+      plot.colour<-base.colour*fraction+peak.colour*(1-fraction)
+      plot.colour<-rgb(plot.colour[1],plot.colour[2],plot.colour[3])
+      plot.colours<-rep(rgb(0,0,0,0),length(land$data))
+      w<-which(land$data>=qtls[level-1] & land$data<=qtls[level])
+      plot.colours[w]<-plot.colour
+      m<-matrix(plot.colours, ncol=length(lons), byrow=T)
+      # flip the data order up<->down to be right for an image
+      #m<-apply(m,2,rev)
+      r.w<-max(lons)-min(lons)+(lons[2]-lons[1])
+      r.c<-(max(lons)+min(lons))/2
+      grid.raster(m,,
+                   x=unit(r.c,'native'),
+                   y=unit(0,'native'),
+                   width=unit(r.w,'native'),
+                   height=unit(180,'native'))
+   }   
+}
+ 
 Draw.temperature<-function(temperature,Options,Trange=1) {
 
   Options.local<-Options
-  Options.local$fog.min.transparency<-0.5
+  Options.local$fog.min.transparency<-0.8
+  Options.local$fog.resolution<-0.25
   tplus<-temperature
-  tplus$data[]<-pmax(0,pmin(Trange,tplus$data))/Trange
+  tplus$data[]<-pmax(0,pmin(Trange,tplus$data))
+  tplus$data[]<-sqrt(tplus$data[])/Trange
   Options.local$fog.colour<-c(1,0,0)
   WeatherMap.draw.fog(tplus,Options.local)
   tminus<-temperature
   tminus$data[]<-tminus$data*-1
-  tminus$data[]<-pmax(0,pmin(Trange,tminus$data))/Trange
+  tminus$data[]<-pmax(0,pmin(Trange,tminus$data))
+  tminus$data[]<-sqrt(tminus$data[])/Trange
   Options.local$fog.colour<-c(0,0,1)
   WeatherMap.draw.fog(tminus,Options.local)
 }
@@ -154,7 +191,7 @@ plot.hour<-function(year,month,day,hour,streamlines) {
 
     ifile.name<-sprintf("%s/%s",Imagedir,image.name)
 
-    land<-WeatherMap.get.land(Options)
+    #land<-WeatherMap.get.land(Options)
     
     t2m<-ERA5.get.slice.at.hour('air.2m',year,month,day,hour)
     t2n<-readRDS(sprintf("%s/ERA5/oper/climtologies.test/air.2m.%02d.Rdata",
@@ -168,8 +205,8 @@ plot.hour<-function(year,month,day,hour,streamlines) {
     prate<-ERA5.get.slice.at.hour('prate',year,month,day,hour)
     prate$data[]<-prate$data/3
      pdf(ifile.name,
-             width=20*16/9,
-             height=20,
+             width=33.1,
+             height=23.4,
              bg=Options$sea.colour,
 	     family='Helvetica',
              pointsize=24)
@@ -187,9 +224,9 @@ plot.hour<-function(year,month,day,hour,streamlines) {
     
       ip<-WeatherMap.rectpoints(Options$ice.points,Options)
       WeatherMap.draw.ice(ip$lat,ip$lon,icec,Options)
-      WeatherMap.draw.land(land,Options)
+      draw.land(Options,n.levels=100)
       WeatherMap.draw.streamlines(streamlines,Options)
-       Draw.temperature(t2m,Options,Trange=100)
+       Draw.temperature(t2m,Options,Trange=10)
        WeatherMap.draw.precipitation(prate,Options)
     Draw.pressure(prmsl.T,Options,colour=c(0,0,0))
     Options$label=sprintf("%04d/%02d/%02d:%02d",year,month,day,hour)
@@ -204,4 +241,4 @@ plot.hour(opt$year,opt$month,opt$day,opt$hour,s)
 image.name<-sprintf("%04d-%02d-%02d:%02d.pdf",
                     opt$year,opt$month,opt$day,opt$hour)
 ifile.name<-sprintf("%s/%s",Imagedir,image.name)
-#embed_fonts(ifile.name)
+embed_fonts(ifile.name)
--- get_data_from_ECMWF/ERA5_fetch.R
+++ get_data_from_ECMWF/ERA5_fetch.R
@@ -8,16 +8,18 @@ library(getopt)
 library(parallel)
 
 opt = getopt(matrix(c(
-  'year',         'y', 2, 'integer',
-  'month',        'm', 2, 'integer',
-  'day',          'd', 2, 'integer',
-  'stream',       's', 2, "character"
+  'year',           'y', 2, 'integer',
+  'month',          'm', 2, 'integer',
+  'day',            'd', 2, 'integer',
+  'pressure.level', 'h', 2, 'logical',
+  'stream',         's', 2, "character"
 ), byrow=TRUE, ncol=4))
 
-if(is.null(opt$year))     stop('Year must be specified')
-if(is.null(opt$month))    stop('Month must be specified')
-if(is.null(opt$day))      stop('Day must be specified')
-if(is.null(opt$stream))   opt$stream='oper'
+if(is.null(opt$year))           stop('Year must be specified')
+if(is.null(opt$month))          stop('Month must be specified')
+if(is.null(opt$day))            stop('Day must be specified')
+if(is.null(opt$stream))         opt$stream='oper'
+if(is.null(opt$pressure.level)) opt$pressure.level=FALSE
 
 local.dir<-sprintf("%s/ERA5/",Sys.getenv('SCRATCH'))
 
@@ -90,3 +92,38 @@ get.surface.forecast<-function(var) {
 }
 
 s<-lapply(vars,get.surface.forecast)
+
+# Pressure level data - analysis
+vars<-c('t','u','v','z','r')
+
+get.pressure.level.analysis<-function(var) {
+ fname<-tempfile()
+ target.dir<-sprintf("%s/%s/hourly/%04d/%02d/%02d",local.dir,opt$stream,opt$year,opt$month,opt$day)
+ if(!file.exists(target.dir)) dir.create(target.dir,recursive=TRUE)
+ target.file<-(sprintf("%s/%s.nc",target.dir,var))
+ if(file.exists(target.file) && file.info(target.file)$size>0) return() # already done
+ sink(fname)
+ cat('#!/usr/bin/env python\n')
+ cat('from ecmwfapi import ECMWFDataServer\n')
+ cat('server = ECMWFDataServer()\n')
+ cat('server.retrieve({\n')
+ cat('   \'dataset\'   : "era5_test",\n')
+ cat(sprintf("   'stream'    : \"%s\",\n",opt$stream))
+ cat('   \'type\'      : "an",\n')
+ cat('   \'levtype\'   : "pl",\n')
+ cat(sprintf("   \'param\'     : \"%s\",\n",var))
+ cat('   \'grid\'      : "0.25/0.25",\n')
+ cat('   \'time\'      : "00/01/02/03/04/05/06/07/08/09/10/11/12/13/14/15/16/17/18/19/20/21/22/23",\n')
+ cat(sprintf("   'date'      : \"%04d-%02d-%02d/to/%04d-%02d-%02d\",\n",
+                               opt$year,opt$month,opt$day,
+			       opt$year,opt$month,opt$day))
+ cat('   \'format\'    : "netcdf",\n')
+ cat(sprintf("   'target'    : \"%s\",\n",target.file))
+ cat('})')
+ sink()
+ system(sprintf("python %s",fname))
+ unlink(fname)
+}
+if(opt$pressure.level) {
+   s<-lapply(vars,get.pressure.level.analysis)
+}
