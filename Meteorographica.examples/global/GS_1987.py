f5ad5af9878df01b61fbb77d5dc70dd8b020597b
Philip Brohan <philip@brohan.org>
2018-02-22 17:37:11 +0000

    Added test air.2m plot.

--- Meteorographica.examples/global/GS_1987.py
+++ Meteorographica.examples/global/GS_1987.py
@@ -58,9 +58,10 @@ ax.set_extent(extent, crs=projection)
 ax.background_patch.set_facecolor((0.88,0.88,0.88,1))
 
 # Add a lat lon grid
-wm.add_grid(ax)
+wm.add_grid(ax,sep_major=5,sep_minor=2.5,color=(0,0.3,0,0.2))
 
 # Plot the land
+matplotlib.rc('image',aspect='auto') # Stop image over-riding aspect
 land_img=ax.background_img(name='GreyT', resolution='low')
 
 # Plot the precip
@@ -75,7 +76,9 @@ prmsl=twcr.get_slice_at_hour('prmsl',year,month,day,hour,
 pe=prmsl.extract(iris.Constraint(member=member))
 pe.data=pe.data/100 # To hPa for labels
 CS=wm.plot_contour(ax,pe,
-                   levels=numpy.arange(870,1050,5),label=True)
+                   levels=numpy.arange(870,1050,5),
+                   colors='black',
+                   fontsize=8,label=True)
 
 # Overplot the wind vectors as a quiver plot
 u=twcr.get_slice_at_hour('uwnd.10m',year,month,day,hour,
@@ -87,16 +90,15 @@ ve=v.extract(iris.Constraint(member=member))
 qv=wm.plot_quiver(ax,ue,ve,scale=1,max_points=100000)
 
 # Add the observations - no obs, removed from NERSC
-#obs=twcr.get_obs_1file(year,month,day,hour,'3.5.1')
-# Filter to those assimilated and near the UK
-#obs_s=obs.loc[(obs['Assimilation.indicator']==1) &
-#              ((obs['Latitude']>0) & (obs['Latitude']<90)) &
-#              ((obs['Longitude']>240) | (obs['Longitude']<100))].copy()
-#wm.plot_obs(ax,obs_s)
+obs=twcr.get_obs_1file(year,month,day,hour,'3.5.1')
+# Filter to those assimilated
+obs_s=obs.loc[(obs['Assimilation.indicator']==1)].copy()
+wm.plot_obs(ax,obs_s,radius=0.3)
 
 # Label the plot with a date
 label=dte.strftime("%A %-d %B %Y at %-H GMT")
-wm.plot_label(ax,label,facecolor=fig.get_facecolor())
+wm.plot_label(ax,label,facecolor=fig.get_facecolor(),
+              x_fraction=0.99)
 
 # Output as png with no border
 fig.savefig('global.%04d-%02d-%02d:%02d.png' % (year,month,day,hour))
