2139647d51156e1cd7f227b088fdc66ea079920e
Philip Brohan <philip@brohan.org>
2018-06-19 17:36:45 +0100

    Updated Meteorographica example to use new module structure.

--- Meteorographica.examples/global/GS_1987.py
+++ Meteorographica.examples/global/GS_1987.py
@@ -12,15 +12,15 @@ import os
 import numpy
 import matplotlib.colors
 from matplotlib.patches import Circle
-import Meteorographica.data.twcr as twcr
+import IRData.twcr as twcr
 import cartopy
 import cartopy.crs as ccrs
 import math
 import pandas
-import virtualtime # fixes datetime to work pre-1900
+#import virtualtime # fixes datetime to work pre-1900
 import datetime
 
-import Meteorographica.weathermap as wm
+import Meteorographica as mg
 
 # Specify the data to plot
 year=1987
@@ -58,47 +58,43 @@ ax.set_extent(extent, crs=projection)
 ax.background_patch.set_facecolor((0.88,0.88,0.88,1))
 
 # Add a lat lon grid
-wm.add_grid(ax,sep_major=5,sep_minor=2.5,color=(0,0.3,0,0.2))
+mg.background.add_grid(ax,sep_major=5,sep_minor=2.5,color=(0,0.3,0,0.2))
 
 # Plot the land
 matplotlib.rc('image',aspect='auto') # Stop image over-riding aspect
 land_img=ax.background_img(name='GreyT', resolution='low')
 
 # Plot the precip
-prate=twcr.get_slice_at_hour('prate',year,month,day,hour,
-                             version='3.5.1',type='ensemble')
+prate=twcr.load('prate',dte,version='2c')
 pe=prate.extract(iris.Constraint(member=member))
-prate_img=wm.plot_cmesh(ax,pe)
+prate_img=mg.precipitation.plot(ax,pe)
 
 # Overplot the pressure as a contour plot
-prmsl=twcr.get_slice_at_hour('prmsl',year,month,day,hour,
-                             version='3.5.1',type='ensemble')
+prmsl=twcr.load('prmsl',dte,version='2c')
 pe=prmsl.extract(iris.Constraint(member=member))
 pe.data=pe.data/100 # To hPa for labels
-CS=wm.plot_contour(ax,pe,
-                   levels=numpy.arange(870,1050,5),
-                   colors='black',
-                   fontsize=8,label=True)
+CS=mg.pressure.plot(ax,pe,type='contour',
+                    levels=numpy.arange(870,1050,5),
+                    colors='black',
+                    fontsize=8,label=True)
 
 # Overplot the wind vectors as a quiver plot
-u=twcr.get_slice_at_hour('uwnd.10m',year,month,day,hour,
-                             version='3.5.1',type='ensemble')
+u=twcr.load('uwnd.10m',dte,version='2c')
 ue=u.extract(iris.Constraint(member=member))
-v=twcr.get_slice_at_hour('vwnd.10m',year,month,day,hour,
-                             version='3.5.1',type='ensemble')
+v=twcr.load('vwnd.10m',dte,version='2c')
 ve=v.extract(iris.Constraint(member=member))
-qv=wm.plot_quiver(ax,ue,ve,scale=1,max_points=100000)
+qv=mg.wind.plot(ax,ue,ve,scale=1,max_points=100000)
 
-# Add the observations - no obs, removed from NERSC
-obs=twcr.get_obs_1file(year,month,day,hour,'3.5.1')
+# Add the observations
+obs=twcr.load_observations_fortime(dte,version='2c')
 # Filter to those assimilated
 obs_s=obs.loc[(obs['Assimilation.indicator']==1)].copy()
-wm.plot_obs(ax,obs_s,radius=0.3)
+mg.observations.plot(ax,obs_s,radius=0.3)
 
 # Label the plot with a date
 label=dte.strftime("%A %-d %B %Y at %-H GMT")
-wm.plot_label(ax,label,facecolor=fig.get_facecolor(),
-              x_fraction=0.99)
+mg.utils.plot_label(ax,label,
+                    facecolor=fig.get_facecolor())
 
 # Output as png with no border
 fig.savefig('global.%04d-%02d-%02d:%02d.png' % (year,month,day,hour))
